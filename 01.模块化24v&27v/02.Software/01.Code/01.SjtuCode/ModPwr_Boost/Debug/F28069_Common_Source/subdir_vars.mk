518a1b2e3a0c2cdcd7aa926c6eaf46d50c4eb812
lishijun3736 <42661286+lishijun3736@users.noreply.github.com>
2018-08-24 09:20:38 +0800

    Add files via upload

---
+++ 01.模块化24v&27v/02.Software/01.Code/01.SjtuCode/ModPwr_Boost/Debug/F28069_Common_Source/subdir_vars.mk
@@ -0,0 +1,176 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+ASM_SRCS += \
+../F28069_Common_Source/F2806x_CodeStartBranch.asm \
+../F28069_Common_Source/F2806x_DBGIER.asm \
+../F28069_Common_Source/F2806x_DisInt.asm \
+../F28069_Common_Source/F2806x_usDelay.asm 
+
+C_SRCS += \
+../F28069_Common_Source/F2806x_Adc.c \
+../F28069_Common_Source/F2806x_Comp.c \
+../F28069_Common_Source/F2806x_CpuTimers.c \
+../F28069_Common_Source/F2806x_DefaultIsr.c \
+../F28069_Common_Source/F2806x_Dma.c \
+../F28069_Common_Source/F2806x_ECan.c \
+../F28069_Common_Source/F2806x_ECap.c \
+../F28069_Common_Source/F2806x_EPwm.c \
+../F28069_Common_Source/F2806x_EQep.c \
+../F28069_Common_Source/F2806x_GlobalVariableDefs.c \
+../F28069_Common_Source/F2806x_Gpio.c \
+../F28069_Common_Source/F2806x_HRCap.c \
+../F28069_Common_Source/F2806x_I2C.c \
+../F28069_Common_Source/F2806x_Mcbsp.c \
+../F28069_Common_Source/F2806x_OscComp.c \
+../F28069_Common_Source/F2806x_PieCtrl.c \
+../F28069_Common_Source/F2806x_PieVect.c \
+../F28069_Common_Source/F2806x_Sci.c \
+../F28069_Common_Source/F2806x_Spi.c \
+../F28069_Common_Source/F2806x_SysCtrl.c \
+../F28069_Common_Source/F2806x_TempSensorConv.c 
+
+OBJS += \
+./F28069_Common_Source/F2806x_Adc.obj \
+./F28069_Common_Source/F2806x_CodeStartBranch.obj \
+./F28069_Common_Source/F2806x_Comp.obj \
+./F28069_Common_Source/F2806x_CpuTimers.obj \
+./F28069_Common_Source/F2806x_DBGIER.obj \
+./F28069_Common_Source/F2806x_DefaultIsr.obj \
+./F28069_Common_Source/F2806x_DisInt.obj \
+./F28069_Common_Source/F2806x_Dma.obj \
+./F28069_Common_Source/F2806x_ECan.obj \
+./F28069_Common_Source/F2806x_ECap.obj \
+./F28069_Common_Source/F2806x_EPwm.obj \
+./F28069_Common_Source/F2806x_EQep.obj \
+./F28069_Common_Source/F2806x_GlobalVariableDefs.obj \
+./F28069_Common_Source/F2806x_Gpio.obj \
+./F28069_Common_Source/F2806x_HRCap.obj \
+./F28069_Common_Source/F2806x_I2C.obj \
+./F28069_Common_Source/F2806x_Mcbsp.obj \
+./F28069_Common_Source/F2806x_OscComp.obj \
+./F28069_Common_Source/F2806x_PieCtrl.obj \
+./F28069_Common_Source/F2806x_PieVect.obj \
+./F28069_Common_Source/F2806x_Sci.obj \
+./F28069_Common_Source/F2806x_Spi.obj \
+./F28069_Common_Source/F2806x_SysCtrl.obj \
+./F28069_Common_Source/F2806x_TempSensorConv.obj \
+./F28069_Common_Source/F2806x_usDelay.obj 
+
+ASM_DEPS += \
+./F28069_Common_Source/F2806x_CodeStartBranch.d \
+./F28069_Common_Source/F2806x_DBGIER.d \
+./F28069_Common_Source/F2806x_DisInt.d \
+./F28069_Common_Source/F2806x_usDelay.d 
+
+C_DEPS += \
+./F28069_Common_Source/F2806x_Adc.d \
+./F28069_Common_Source/F2806x_Comp.d \
+./F28069_Common_Source/F2806x_CpuTimers.d \
+./F28069_Common_Source/F2806x_DefaultIsr.d \
+./F28069_Common_Source/F2806x_Dma.d \
+./F28069_Common_Source/F2806x_ECan.d \
+./F28069_Common_Source/F2806x_ECap.d \
+./F28069_Common_Source/F2806x_EPwm.d \
+./F28069_Common_Source/F2806x_EQep.d \
+./F28069_Common_Source/F2806x_GlobalVariableDefs.d \
+./F28069_Common_Source/F2806x_Gpio.d \
+./F28069_Common_Source/F2806x_HRCap.d \
+./F28069_Common_Source/F2806x_I2C.d \
+./F28069_Common_Source/F2806x_Mcbsp.d \
+./F28069_Common_Source/F2806x_OscComp.d \
+./F28069_Common_Source/F2806x_PieCtrl.d \
+./F28069_Common_Source/F2806x_PieVect.d \
+./F28069_Common_Source/F2806x_Sci.d \
+./F28069_Common_Source/F2806x_Spi.d \
+./F28069_Common_Source/F2806x_SysCtrl.d \
+./F28069_Common_Source/F2806x_TempSensorConv.d 
+
+C_DEPS__QUOTED += \
+"F28069_Common_Source\F2806x_Adc.d" \
+"F28069_Common_Source\F2806x_Comp.d" \
+"F28069_Common_Source\F2806x_CpuTimers.d" \
+"F28069_Common_Source\F2806x_DefaultIsr.d" \
+"F28069_Common_Source\F2806x_Dma.d" \
+"F28069_Common_Source\F2806x_ECan.d" \
+"F28069_Common_Source\F2806x_ECap.d" \
+"F28069_Common_Source\F2806x_EPwm.d" \
+"F28069_Common_Source\F2806x_EQep.d" \
+"F28069_Common_Source\F2806x_GlobalVariableDefs.d" \
+"F28069_Common_Source\F2806x_Gpio.d" \
+"F28069_Common_Source\F2806x_HRCap.d" \
+"F28069_Common_Source\F2806x_I2C.d" \
+"F28069_Common_Source\F2806x_Mcbsp.d" \
+"F28069_Common_Source\F2806x_OscComp.d" \
+"F28069_Common_Source\F2806x_PieCtrl.d" \
+"F28069_Common_Source\F2806x_PieVect.d" \
+"F28069_Common_Source\F2806x_Sci.d" \
+"F28069_Common_Source\F2806x_Spi.d" \
+"F28069_Common_Source\F2806x_SysCtrl.d" \
+"F28069_Common_Source\F2806x_TempSensorConv.d" 
+
+OBJS__QUOTED += \
+"F28069_Common_Source\F2806x_Adc.obj" \
+"F28069_Common_Source\F2806x_CodeStartBranch.obj" \
+"F28069_Common_Source\F2806x_Comp.obj" \
+"F28069_Common_Source\F2806x_CpuTimers.obj" \
+"F28069_Common_Source\F2806x_DBGIER.obj" \
+"F28069_Common_Source\F2806x_DefaultIsr.obj" \
+"F28069_Common_Source\F2806x_DisInt.obj" \
+"F28069_Common_Source\F2806x_Dma.obj" \
+"F28069_Common_Source\F2806x_ECan.obj" \
+"F28069_Common_Source\F2806x_ECap.obj" \
+"F28069_Common_Source\F2806x_EPwm.obj" \
+"F28069_Common_Source\F2806x_EQep.obj" \
+"F28069_Common_Source\F2806x_GlobalVariableDefs.obj" \
+"F28069_Common_Source\F2806x_Gpio.obj" \
+"F28069_Common_Source\F2806x_HRCap.obj" \
+"F28069_Common_Source\F2806x_I2C.obj" \
+"F28069_Common_Source\F2806x_Mcbsp.obj" \
+"F28069_Common_Source\F2806x_OscComp.obj" \
+"F28069_Common_Source\F2806x_PieCtrl.obj" \
+"F28069_Common_Source\F2806x_PieVect.obj" \
+"F28069_Common_Source\F2806x_Sci.obj" \
+"F28069_Common_Source\F2806x_Spi.obj" \
+"F28069_Common_Source\F2806x_SysCtrl.obj" \
+"F28069_Common_Source\F2806x_TempSensorConv.obj" \
+"F28069_Common_Source\F2806x_usDelay.obj" 
+
+ASM_DEPS__QUOTED += \
+"F28069_Common_Source\F2806x_CodeStartBranch.d" \
+"F28069_Common_Source\F2806x_DBGIER.d" \
+"F28069_Common_Source\F2806x_DisInt.d" \
+"F28069_Common_Source\F2806x_usDelay.d" 
+
+C_SRCS__QUOTED += \
+"../F28069_Common_Source/F2806x_Adc.c" \
+"../F28069_Common_Source/F2806x_Comp.c" \
+"../F28069_Common_Source/F2806x_CpuTimers.c" \
+"../F28069_Common_Source/F2806x_DefaultIsr.c" \
+"../F28069_Common_Source/F2806x_Dma.c" \
+"../F28069_Common_Source/F2806x_ECan.c" \
+"../F28069_Common_Source/F2806x_ECap.c" \
+"../F28069_Common_Source/F2806x_EPwm.c" \
+"../F28069_Common_Source/F2806x_EQep.c" \
+"../F28069_Common_Source/F2806x_GlobalVariableDefs.c" \
+"../F28069_Common_Source/F2806x_Gpio.c" \
+"../F28069_Common_Source/F2806x_HRCap.c" \
+"../F28069_Common_Source/F2806x_I2C.c" \
+"../F28069_Common_Source/F2806x_Mcbsp.c" \
+"../F28069_Common_Source/F2806x_OscComp.c" \
+"../F28069_Common_Source/F2806x_PieCtrl.c" \
+"../F28069_Common_Source/F2806x_PieVect.c" \
+"../F28069_Common_Source/F2806x_Sci.c" \
+"../F28069_Common_Source/F2806x_Spi.c" \
+"../F28069_Common_Source/F2806x_SysCtrl.c" \
+"../F28069_Common_Source/F2806x_TempSensorConv.c" 
+
+ASM_SRCS__QUOTED += \
+"../F28069_Common_Source/F2806x_CodeStartBranch.asm" \
+"../F28069_Common_Source/F2806x_DBGIER.asm" \
+"../F28069_Common_Source/F2806x_DisInt.asm" \
+"../F28069_Common_Source/F2806x_usDelay.asm" 
+
+
